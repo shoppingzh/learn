056f7a3ee0486db4e05f7608bca9980bebd15aa4
shoppingzh <xpzheng6@163.com>
2023-09-13 11:41:16 +0800

    feat: 学习pdfjs

--- pdfjs/index.js
+++ pdfjs/index.js
@@ -3,10 +3,25 @@ import fs from 'fs'
 
 const buffer = fs.readFileSync('./1.pdf')
 
+let html = ``
+
 ;(async() => {
 
   const pdf = await pdfjs.getDocument(new Uint8Array(buffer)).promise
 
-  console.log(pdf.numPages)
+  for (let i = 1; i <= pdf.numPages; i++) {
+    const page = await pdf.getPage(i)
+    
+    const content = await page.getTextContent({
+      includeMarkedContent: true,
+    })
+    // const tree = await page.getStructTree()
+    console.log(content);
+    // content.items.forEach(o => {
+    //   console.log(o.str);
+    //   html += o.str
+    // })
+  }
 
+  fs.writeFileSync('./result.txt', html)
 })()
