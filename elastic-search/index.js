74fe7a0dba9ba478be84b615e4e4d237daeceda3
shoppingzh <xpzheng6@163.com>
2023-09-15 14:24:24 +0800

    feat: 学习es

--- elastic-search/index.js
+++ elastic-search/index.js
@@ -1,5 +1,48 @@
 import { Client } from '@elastic/elasticsearch'
 
-new Client({
-  
+const client = new Client({
+  node: 'http://127.0.0.1:9200'
 })
+
+;(async() => {
+  await client.ping()
+
+  if (!client.indices.exists({
+    index: 'first_index'
+  })) {
+    await client.indices.create({
+      index: 'first_index'
+    })
+  }
+
+
+  await client.index({
+    index: 'first_index',
+    // id: 'zxp',
+    document: {
+      name: 'zxp',
+      age: 20,
+      gender: true,
+      address: {
+        province: 'Guandong',
+        city: 'Shenzhen',
+      }
+    }
+  })
+
+  const doc = await client.get({
+    index: 'first_index',
+    id: 'zxp'
+  })
+  console.log(doc);
+
+  const result = await client.search({
+    query: {
+      match: {
+        name: 'zxp'
+      }
+    }
+  })
+  console.log(result)
+
+})()
