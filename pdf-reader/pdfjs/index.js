ffd9e5761b084e5cd8d6cd1a3e5f84f47a82f4f2
shoppingzh <xpzheng6@163.com>
2023-09-15 18:13:43 +0800

    feat: 学习pdfjs解析

--- pdf-reader/pdfjs/index.js
+++ pdf-reader/pdfjs/index.js
@@ -1,26 +1,10 @@
-import pdfjs from 'pdfjs-dist'
 import fs from 'fs'
+import { parse } from './parser.js'
 
-const buffer = fs.readFileSync('./1.pdf')
-
-let html = ``
 
 ;(async() => {
 
-  const pdf = await pdfjs.getDocument(new Uint8Array(buffer)).promise
-
-  for (let i = 1; i <= pdf.numPages; i++) {
-    const page = await pdf.getPage(i)
-
-    const content = await page.getTextContent({
-      includeMarkedContent: true,
-    })
-    // const tree = await page.getStructTree()
-    // console.log(content);
-    content.items.forEach(o => {
-      html += o.str
-    })
-  }
+  const html = await parse('./1.pdf')
 
   fs.writeFileSync('./result.txt', html, { encoding: 'utf-8' })
 })()
