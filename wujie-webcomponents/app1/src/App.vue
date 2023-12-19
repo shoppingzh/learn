8e64fb3c568a51414fb41ad2a5e7380b8004485c
shoppingzh <xpzheng6@163.com>
2023-12-19 16:11:22 +0800

    feat: 学习无界嵌套webcomponents的问题

--- wujie-webcomponents/app1/src/App.vue
+++ wujie-webcomponents/app1/src/App.vue
@@ -19,16 +19,11 @@ customElements.define('my-link',
       a.href = 'https://google.com'
       a.innerHTML = '链接'
 
-      // const link = document.createElement('link')
-      // link.rel = 'stylesheet'
-      // link.href = style
-
       const sheet = new CSSStyleSheet()
       sheet.replaceSync(style)
 
       const shadowRoot = this.attachShadow({mode: 'open'})
       shadowRoot.adoptedStyleSheets = [sheet]
-      // shadowRoot.appendChild(link)
       shadowRoot.appendChild(
         a
       )
--- wujie-webcomponents/root/src/App.vue
+++ wujie-webcomponents/root/src/App.vue
@@ -2,11 +2,16 @@
 
 <template>
   <WujieVue
-    url="http://100.119.120.55:5174" />
+    url="http://100.119.120.55:5174"
+    :beforeLoad="beforeLoad" />
 </template>
 
 <script setup lang="ts">
 import WujieVue from 'wujie-vue3'
+
+function beforeLoad(appWindow: any) { 
+  appWindow.CSSStyleSheet = appWindow.top.CSSStyleSheet
+}
 </script>
 
 <style scoped>
