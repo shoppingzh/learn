3499f08e381f8029766a2b5fc4815b07e7cd1c50
shoppingzh <xpzheng6@163.com>
2024-01-02 11:48:33 +0800

    feat: 学习es Proxy

---
+++ es/src/proxy.js
@@ -0,0 +1,42 @@
+function proxyLogFunction(value) {
+  return new Proxy(value, {
+    async apply(func, thisArg, args) {
+      const start = +new Date()
+      const result = await func.apply(thisArg, args)
+      console.log(`调用时间：${+new Date() - start}ms`)
+      expect(1).toBe(1)
+      return result
+    }
+  })
+}
+
+it('get', () => {
+  let times = 0
+  const proxy = new Proxy({
+    a: 1
+  }, {
+    get(target, prop) {
+      if (prop === 'times') return times++
+      return target[prop]
+    }
+  })
+
+  expect(proxy.a).toBe(1)
+  expect(proxy.times).toBe(1)
+  expect(proxy.times).toBe(2)
+  expect(proxy.times).toBe(3)
+})
+
+it('function', async() => {
+
+  async function hello() {
+    await new Promise(r => {
+      setTimeout(() => {
+        console.log('hello')
+        r()
+      }, 10)
+    })
+  }
+  const proxyHello = proxyLogFunction(hello)
+  await proxyHello()
+})
