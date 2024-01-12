11e23e97e8fc7313bf2e8c74d9f3f507ad017140
shoppingzh <xpzheng6@163.com>
2024-01-12 15:50:38 +0800

    feat: 学习es unicode

--- es/src/data-types/string.js
+++ es/src/data-types/string.js
@@ -4,3 +4,7 @@ it('padStart', () => {
   expect('hello'.padStart(8, '/')).toBe('///hello')
   expect('hello'.padStart(4)).toBe('hello')
 })
+
+it('unicode', () => {
+  expect('😄'.codePointAt(0)).toBe(128516)
+})
