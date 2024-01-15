a2268f02ffc87e5215615c88328daa31229af0b0
Shopping <xpzheng6@163.com>
2024-01-15 21:59:47 +0800

    feat: 学习es 模板字符串

--- es/src/data-types/string.js
+++ es/src/data-types/string.js
@@ -8,3 +8,25 @@ it('padStart', () => {
 it('unicode', () => {
   expect('😄'.codePointAt(0)).toBe(128516)
 })
+
+it('template', () => {
+  function rm(strings, ...exps) {
+    return strings.join('')
+  }
+  expect(rm`hello${','} I'm ${'xp'}`).toBe('hello I\'m ' )
+})
+
+it('String.raw', () => {
+  expect(String.raw`c:\1.txt`).toBe('c:\\1.txt')
+})
+
+it('original', () => {
+  function original(strings, ...exps) {
+    return strings.map((str, i) => {
+      if (i > exps.length - 1) return str
+      return str + exps[i]
+    }).join('')
+  }
+  expect(original`abc, ${1}, xyz, ${2}, ${'hello'}, nice, ${undefined}`)
+    .toBe(`abc, ${1}, xyz, ${2}, ${'hello'}, nice, ${undefined}`)
+})
