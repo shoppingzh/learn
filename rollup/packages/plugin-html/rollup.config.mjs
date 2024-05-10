604ce54968b03f14840028f991a669329664f757
shoppingzh <xpzheng6@163.com>
2024-05-10 17:43:00 +0800

    feat: 学习rollup插件

--- rollup/packages/plugin-html/rollup.config.mjs
+++ rollup/packages/plugin-html/rollup.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'rollup'
 import html from '@rollup/plugin-html'
+import fs from 'fs'
 
 export default defineConfig({
   input: './src/index.js',
@@ -8,6 +9,17 @@ export default defineConfig({
     format: 'esm',
   },
   plugins: [
-    html({})
+    html({
+      title: 'Hello',
+      attributes: {
+        html: { lang: 'zh_CN' },
+      },
+      fileName: 'hello.html',
+      meta: [{ icon: 'hello' }, { a: 'b' }],
+      // template: (data) => {
+      //   console.log(data.files);
+      //   return fs.readFileSync('./src/template.html', { encoding: 'utf-8' })
+      // }
+    })
   ]
 })
